ae86b49b57f6f8617055accd220a51f5d42e7a06
Alopex6414 <1029535012@qq.com>
2017-12-28 11:19:57 +0800

    Bracelet Project

---
+++ Bracelet/FormMain.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace Bracelet
+{
+    public partial class FormMain : Form
+    {
+        [DllImport("User32", EntryPoint = "GetSystemMetrics")]
+        private static extern int GetSystemMetrics(int nIndex);
+        [DllImport("User32", EntryPoint = "SetWindowPos")]
+        private static extern int SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, int uFlags);
+
+        #region 窗口初始化函数
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region 窗口加载函数
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            FormMain_InitWindow();//窗口位置初始化
+        }
+        #endregion
+
+        #region 窗口位置初始化函数
+        private void FormMain_InitWindow()
+        {
+            int nX = 0;
+            int nY = 0;
+            int nScreenWidth = 0;
+            int nScreenHeight = 0;
+
+            this.Width = 960;   //窗口宽度
+            this.Height = 720;  //窗口高度
+
+            nScreenWidth = GetSystemMetrics(0);     //屏幕宽度
+            nScreenHeight = GetSystemMetrics(1);    //屏幕高度
+
+            if(nScreenWidth >= this.Width && nScreenHeight >= this.Height)
+            {
+                nX = (nScreenWidth - this.Width) >> 1;
+                nY = (nScreenHeight - this.Height) >> 1;
+                SetWindowPos(this.Handle, (IntPtr)0, nX, nY, this.Width, this.Height, 64);
+            }
+        }
+        #endregion
+    }
+}
