5e6eb4bcf55f371cc01db4912ab75e2effe0b48a
Helium <1029535012@qq.com>
2018-02-01 13:33:15 +0800

    Make Window Attribute.

--- Bracelet/FormMain.Designer.cs
+++ Bracelet/FormMain.Designer.cs
@@ -28,21 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.FormMain_Panel_Min = new System.Windows.Forms.Panel();
+            this.FormMain_Panel_Max = new System.Windows.Forms.Panel();
+            this.FormMain_Panel_Close = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // FormMain_Panel_Min
+            // 
+            this.FormMain_Panel_Min.BackgroundImage = global::Bracelet.Properties.Resources.min;
+            this.FormMain_Panel_Min.Location = new System.Drawing.Point(858, 0);
+            this.FormMain_Panel_Min.Name = "FormMain_Panel_Min";
+            this.FormMain_Panel_Min.Size = new System.Drawing.Size(34, 34);
+            this.FormMain_Panel_Min.TabIndex = 2;
+            this.FormMain_Panel_Min.Click += new System.EventHandler(this.FormMain_Panel_Min_Click);
+            this.FormMain_Panel_Min.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FormMain_Panel_Min_MouseDown);
+            this.FormMain_Panel_Min.MouseEnter += new System.EventHandler(this.FormMain_Panel_Min_MouseEnter);
+            this.FormMain_Panel_Min.MouseLeave += new System.EventHandler(this.FormMain_Panel_Min_MouseLeave);
+            this.FormMain_Panel_Min.MouseUp += new System.Windows.Forms.MouseEventHandler(this.FormMain_Panel_Min_MouseUp);
+            // 
+            // FormMain_Panel_Max
+            // 
+            this.FormMain_Panel_Max.BackgroundImage = global::Bracelet.Properties.Resources.max;
+            this.FormMain_Panel_Max.Location = new System.Drawing.Point(892, 0);
+            this.FormMain_Panel_Max.Name = "FormMain_Panel_Max";
+            this.FormMain_Panel_Max.Size = new System.Drawing.Size(34, 34);
+            this.FormMain_Panel_Max.TabIndex = 1;
+            this.FormMain_Panel_Max.Click += new System.EventHandler(this.FormMain_Panel_Max_Click);
+            this.FormMain_Panel_Max.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FormMain_Panel_Max_MouseDown);
+            this.FormMain_Panel_Max.MouseEnter += new System.EventHandler(this.FormMain_Panel_Max_MouseEnter);
+            this.FormMain_Panel_Max.MouseLeave += new System.EventHandler(this.FormMain_Panel_Max_MouseLeave);
+            this.FormMain_Panel_Max.MouseUp += new System.Windows.Forms.MouseEventHandler(this.FormMain_Panel_Max_MouseUp);
+            // 
+            // FormMain_Panel_Close
+            // 
+            this.FormMain_Panel_Close.BackgroundImage = global::Bracelet.Properties.Resources.close;
+            this.FormMain_Panel_Close.Location = new System.Drawing.Point(926, 0);
+            this.FormMain_Panel_Close.Name = "FormMain_Panel_Close";
+            this.FormMain_Panel_Close.Size = new System.Drawing.Size(34, 34);
+            this.FormMain_Panel_Close.TabIndex = 0;
+            this.FormMain_Panel_Close.Click += new System.EventHandler(this.FormMain_Panel_Close_Click);
+            this.FormMain_Panel_Close.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FormMain_Panel_Close_MouseDown);
+            this.FormMain_Panel_Close.MouseEnter += new System.EventHandler(this.FormMain_Panel_Close_MouseEnter);
+            this.FormMain_Panel_Close.MouseLeave += new System.EventHandler(this.FormMain_Panel_Close_MouseLeave);
+            this.FormMain_Panel_Close.MouseUp += new System.Windows.Forms.MouseEventHandler(this.FormMain_Panel_Close_MouseUp);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.ClientSize = new System.Drawing.Size(960, 720);
+            this.Controls.Add(this.FormMain_Panel_Min);
+            this.Controls.Add(this.FormMain_Panel_Max);
+            this.Controls.Add(this.FormMain_Panel_Close);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormMain";
             this.Text = "FormMain";
             this.Load += new System.EventHandler(this.FormMain_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormMain_Paint);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel FormMain_Panel_Close;
+        private System.Windows.Forms.Panel FormMain_Panel_Max;
+        private System.Windows.Forms.Panel FormMain_Panel_Min;
     }
 }
 
--- Bracelet/FormMain.cs
+++ Bracelet/FormMain.cs
@@ -19,17 +19,22 @@ namespace Bracelet
         [DllImport("User32", EntryPoint = "SetWindowPos")]
         private static extern int SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, int uFlags);
 
+        private Rectangle m_MainRect;       //窗口矩形区域
+        private Rectangle m_ToolRect;       //窗口工具栏矩形区域
+
         #region 窗口初始化函数
         public FormMain()
         {
-            InitializeComponent();
+            InitializeComponent();//窗口初始化(Native)
         }
         #endregion
 
         #region 窗口加载函数
         private void FormMain_Load(object sender, EventArgs e)
         {
-            FormMain_InitWindow();//窗口位置初始化
+            FormMain_InitWindow();//窗口位置形状初始化
+            FormMain_InitWindowTool();//窗口工具栏形状初始化
+            FormMain_InitWindowAttribute();//窗口属性初始化
         }
         #endregion
 
@@ -54,10 +59,146 @@ namespace Bracelet
                 SetWindowPos(this.Handle, (IntPtr)0, nX, nY, this.Width, this.Height, 64);  //设置窗口初始位置
             }
 
+            m_MainRect.X = 0;
+            m_MainRect.Y = 0;
+            m_MainRect.Width = this.Width;
+            m_MainRect.Height = this.Height;
+        }
+        #endregion
+
+        #region 窗口工具栏形状初始化
+        private void FormMain_InitWindowTool()
+        {
+            m_ToolRect.X = m_MainRect.X;
+            m_ToolRect.Y = m_MainRect.Y;
+            m_ToolRect.Width = 64;
+            m_ToolRect.Height = m_MainRect.Height;
+        }
+        #endregion
+
+        #region 窗口属性初始化
+        private void FormMain_InitWindowAttribute()
+        {
             this.DoubleBuffered = true; //窗口双缓冲
-            this.FormBorderStyle = FormBorderStyle.FixedSingle; //窗口禁止改变大小
+            this.FormBorderStyle = FormBorderStyle.None; //窗口无边框
             this.MaximizeBox = false; //窗体禁止最大化
         }
         #endregion
+
+        #region 窗口样式初始化设置函数
+        private void FormMain_SetWindowStyle()
+        {
+
+        }
+        #endregion
+
+        #region 窗口重绘
+        private void FormMain_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            SolidBrush SolidBrushMain = new SolidBrush(Color.FromArgb(245, 245, 245));
+            SolidBrush SolidBrushTool = new SolidBrush(Color.FromArgb(39, 42, 47));
+
+            graphics.FillRectangle(SolidBrushMain, m_MainRect);
+            graphics.FillRectangle(SolidBrushTool, m_ToolRect);
+        }
+        #endregion
+
+        #region 关闭按钮
+        //FormMain_Panel_Close进入
+        private void FormMain_Panel_Close_MouseEnter(object sender, EventArgs e)
+        {
+            this.FormMain_Panel_Close.BackgroundImage = Properties.Resources.close_hor;
+        }
+
+        //FormMain_Panel_Close离开
+        private void FormMain_Panel_Close_MouseLeave(object sender, EventArgs e)
+        {
+            this.FormMain_Panel_Close.BackgroundImage = Properties.Resources.close;
+        }
+
+        //FormMain_Panel_Close按下
+        private void FormMain_Panel_Close_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.FormMain_Panel_Close.BackgroundImage = Properties.Resources.close_focus;
+        }
+
+        //FormMain_Panel_Close释放
+        private void FormMain_Panel_Close_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.FormMain_Panel_Close.BackgroundImage = Properties.Resources.close_hor;
+        }
+
+        //FormMain_Panel_Close单击
+        private void FormMain_Panel_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+
+        #region 最大化按钮
+        //FormMain_Panel_Max进入
+        private void FormMain_Panel_Max_MouseEnter(object sender, EventArgs e)
+        {
+            this.FormMain_Panel_Max.BackgroundImage = Properties.Resources.max_hor;
+        }
+
+        //FormMain_Panel_Max离开
+        private void FormMain_Panel_Max_MouseLeave(object sender, EventArgs e)
+        {
+            this.FormMain_Panel_Max.BackgroundImage = Properties.Resources.max;
+        }
+
+        //FormMain_Panel_Max按下
+        private void FormMain_Panel_Max_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.FormMain_Panel_Max.BackgroundImage = Properties.Resources.max_focus;
+        }
+
+        //FormMain_Panel_Max释放
+        private void FormMain_Panel_Max_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.FormMain_Panel_Max.BackgroundImage = Properties.Resources.max_hor;
+        }
+
+        //FormMain_Panel_Max单击
+        private void FormMain_Panel_Max_Click(object sender, EventArgs e)
+        {
+           //this.WindowState = FormWindowState.Normal;
+        }
+        #endregion
+
+        #region 最小化按钮
+        //FormMain_Panel_Min进入
+        private void FormMain_Panel_Min_MouseEnter(object sender, EventArgs e)
+        {
+            this.FormMain_Panel_Min.BackgroundImage = Properties.Resources.min_hor;
+        }
+
+        //FormMain_Panel_Min离开
+        private void FormMain_Panel_Min_MouseLeave(object sender, EventArgs e)
+        {
+            this.FormMain_Panel_Min.BackgroundImage = Properties.Resources.min;
+        }
+
+        //FormMain_Panel_Min按下
+        private void FormMain_Panel_Min_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.FormMain_Panel_Min.BackgroundImage = Properties.Resources.min_focus;
+        }
+
+        //FormMain_Panel_Min释放
+        private void FormMain_Panel_Min_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.FormMain_Panel_Min.BackgroundImage = Properties.Resources.min_hor;
+        }
+
+        //FormMain_Panel_Min单击
+        private void FormMain_Panel_Min_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        #endregion
+
     }
 }
